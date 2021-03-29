6326d832d4ebea2a5b96a92ab35037221a82c561
Samuel Quiñones <sdquinones1@gmail.com>
2021-03-29 18:57:53 -0400

    updated NextJS and enabled webpack5 flag

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 // These configurations don't particularly mean anything, they are here to illustrate how you could use this file.
 
 module.exports = {
+  /** Enables webpack5 features, more info here: https://nextjs.org/docs/messages/webpack5 */
+  future: {
+    webpack5: true,
+  },
   /** By default Next.js will add the `x-powered-by` header. This will opt-out of it */
   poweredByHeader: false,
   /** Headers allow you to set custom HTTP headers for an incoming request path. */
